e918e015c5a163a0921c4b28c4a79045740eda5f
AriefDaffa <ariefkeren76@yahoo.com>
2024-02-03 06:41:31 +0700

    style: update tourist detail card

--- src/components/Flexer/index.tsx
+++ src/components/Flexer/index.tsx
@@ -4,18 +4,21 @@ interface FlexerProps {
   children: ReactNode;
   className?: string;
   flexDirection?: 'row' | 'col';
+  onClick?: () => void;
 }
 
 const Flexer: FC<FlexerProps> = ({
   children,
   className,
   flexDirection = 'row',
+  onClick = () => {},
 }) => {
   return (
     <div
       className={`flex ${
         flexDirection === 'row' ? 'flex-row' : 'flex-col'
       } ${className}`}
+      onClick={onClick}
     >
       {children}
     </div>
--- src/pages/TouristDetail/index.tsx
+++ src/pages/TouristDetail/index.tsx
@@ -1,20 +1,19 @@
-import { useState } from 'react';
-import { IoMdArrowBack } from 'react-icons/io';
+import { IoChevronBack } from 'react-icons/io5';
+import { FaLocationDot } from 'react-icons/fa6';
+import { IoMdMail } from 'react-icons/io';
 import { useNavigate, useParams } from 'react-router-dom';
+
 import type { FC } from 'react';
 
 import Container from '@/components/Container';
 import Navbar from '@/components/Navbar';
 import Flexer from '@/components/Flexer';
-import Button from '@/components/Button';
 import useGetTouristDetail from '@/repository/tourist/detail-tourist/useGetTouristDetail';
 import { useUserContext } from '@/context/UserContext';
 
 interface TouristDetailProps {}
 
 const TouristDetail: FC<TouristDetailProps> = () => {
-  const [activateUpdate, setActivateUpdate] = useState(false);
-
   const { user } = useUserContext();
   const navigate = useNavigate();
   const params = useParams();
@@ -26,23 +25,51 @@ const TouristDetail: FC<TouristDetailProps> = () => {
     token: user.Token,
   });
 
-  const updateMode = () => {
-    setActivateUpdate(!activateUpdate);
-  };
-
   return (
     <Container className="px-2 h-screen pt-24 md:px-4">
       <Navbar />
       <div>
-        <IoMdArrowBack
-          size={22}
-          className="cursor-pointer"
+        <Flexer
           onClick={() => navigate('/tourists')}
-        />
+          className="gap-1 items-center w-min rounded-lg pr-2 text-gray-500 cursor-pointer hover:bg-gray-200"
+        >
+          <IoChevronBack size={18} className="cursor-pointer" />
+          <div className="font-medium">Back</div>
+        </Flexer>
       </div>
       <Flexer className="justify-center gap-4 items-center">
-        <Flexer className="bg-white p-2 rounded-lg relative gap-2 w-5/6 flex-col md:flex-row md:w3/4">
-          <Flexer className="justify-center">
+        <Flexer
+          flexDirection="col"
+          className="bg-white p-2 rounded-lg relative gap-2 items-center md:w-3/4"
+        >
+          <div className="h-full w-64">
+            <img
+              src={data.tourist_profilepicture}
+              alt=""
+              className="rounded-lg w-full h-full object-cover"
+            />
+          </div>
+          <div className="text-center py-4">
+            <div className="text-xs text-gray-500">{data.id}</div>
+            <div className="font-semibold text-4xl py-2">
+              {data.tourist_name}
+            </div>
+            <Flexer className="gap-2 flex-col w-full items-center md:flex-row">
+              <Flexer className="text-sm text-gray-500 items-center gap-1">
+                <span>
+                  <IoMdMail />
+                </span>
+                {data.tourist_email}
+              </Flexer>
+              <Flexer className="text-sm text-gray-500 items-center gap-1">
+                <span>
+                  <FaLocationDot />
+                </span>
+                {data.tourist_location}
+              </Flexer>
+            </Flexer>
+          </div>
+          {/* <Flexer className="justify-center">
             <div className="h-full w-64">
               <img
                 src={data.tourist_profilepicture}
@@ -50,8 +77,8 @@ const TouristDetail: FC<TouristDetailProps> = () => {
                 className="rounded-lg w-full h-full object-cover"
               />
             </div>
-          </Flexer>
-          <Flexer flexDirection="col" className="justify-between w-full">
+          </Flexer> */}
+          {/* <Flexer flexDirection="col" className="justify-between w-full">
             <Flexer flexDirection="col" className="gap-1 w-full">
               <label className="text-sm text-gray-500">Name</label>
               {activateUpdate ? (
@@ -101,7 +128,7 @@ const TouristDetail: FC<TouristDetailProps> = () => {
               />
               {activateUpdate && <Button text="Submit" onClick={updateMode} />}
             </Flexer>
-          </Flexer>
+          </Flexer> */}
         </Flexer>
       </Flexer>
     </Container>
